c9633fc2cb4b91cdc535b326c86171b38317fec5
vikasreddy636 <73032093+vikasreddy636@users.noreply.github.com>
2021-02-23 21:04:27 +0100

    Add files via upload

---
+++ ARP_headers/handshake/version.h
@@ -0,0 +1,9 @@
+#ifndef _ARPNET_LIB_VERSION_H_
+#define _ARPNET_LIB_VERSION_H_
+
+// the version identifier of the version is a n characters random generated string
+// the final string wll be generated with the final version
+#define __ARPNET_VER_ID_LEN 37
+#define __ARPNET_VER_ID "f71gn41n02FE4BA1lj7sw53q15l74kf11as9"
+/*	Last modified Feb-19-2021	*/
+#endif
